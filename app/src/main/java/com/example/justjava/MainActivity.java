883c6b21713882ed2bc784c142f2e45c742455d5
WAHYUPRANATA <62340335+WAHYUPRANATA@users.noreply.github.com>
2020-10-15 16:17:22 +0700

    rangkaian string

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-int quantity = 2;
+int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +29,17 @@ int quantity = 2;
     }
 
     public void submitOrder (View view){
-        displayPrice(quantity*5000)
+        int price = quantity * 5000;
+        String priceMessage ="Total pembelian Rp " + price;
+        priceMessage = priceMessage + "\nTerimakasih";
+        displayMessage(priceMessage);
     }
+
+    private void displayMessage(String message) {
+        TextView priceTextView =(TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
     private void display(int number){
         TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
